409514af155ac4eca869085e68076466f717109b
Bilhid <bil.bilhid09@gmail.com>
2021-07-28 14:35:28 +0200

    added show user page clean some code, gravatar and min-height setted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id]) 
+    end
+    
+
+
     def new
         @user = User.new
     end    
+
+
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -11,6 +21,27 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+
+
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+
+
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Welcome to the Blogenesis #{@user.username}"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
+    
 
 
 
