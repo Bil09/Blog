124c3f5ffe919f220c5b85a8b751068ac363c963
Bilhid <bil.bilhid09@gmail.com>
2021-07-30 16:17:22 +0200

    login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
@@ -8,6 +9,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   resources :users, expect: [:new]
+  get    'login',   to: 'sessions#new'
+  post   'login',   to: 'sessions#create'
+  delete 'logout',  to: 'sessions#destroy'
+
 
 
 end
