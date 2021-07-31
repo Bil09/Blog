4fcde025102220cef0c08523b0043b6b42bafb02
Bilhid <bil.bilhid09@gmail.com>
2021-07-31 22:28:26 +0200

    update create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
           flash[:notice] = "Hey, the article was successfully created"
           redirect_to article_path(@article)
