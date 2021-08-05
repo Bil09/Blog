86f0b88f4b9dea0e13e65a5dd6b0a26050330b7e
Bilhid <bil.bilhid09@gmail.com>
2021-08-05 15:26:25 +0200

    fix test and feature for admin user in the navbar

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 class CategoriesTest < ActionDispatch::IntegrationTest
-    
+      
+   def setup
+    @user = User.create(username: "joe", email: "joe@example.com", password: "password", admin: true)
+   end
+   
+
+
     test "get new category form and create new category" do
+        sign_in_as(@user,"password")
         get new_category_path
         assert_template 'categories/new'
         assert_difference "Category.count", 1 do
@@ -13,6 +20,7 @@ class CategoriesTest < ActionDispatch::IntegrationTest
     end
 
     test "invald category submission should result in a failure" do
+        sign_in_as(@user,"password")
         get new_category_path
         assert_template 'categories/new'
         assert_no_difference "Category.count" do
